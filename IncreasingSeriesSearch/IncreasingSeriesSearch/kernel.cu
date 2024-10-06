264419c748d9de830647d0714121c5330b1a3dcf
Oláh Levente <levente@stud.uni-obuda.hu>
2024-10-06 14:32:32 +0200

    made it work for blocks

--- IncreasingSeriesSearch/IncreasingSeriesSearch/kernel.cu
+++ IncreasingSeriesSearch/IncreasingSeriesSearch/kernel.cu
@@ -6,8 +6,8 @@
 #include <time.h>
 
 #define BLOCK_SIZE 500
-#define N 200
-#define K 5
+#define N 10499
+#define K 10
 
 
 int numArray[N];
@@ -19,15 +19,33 @@ __device__ bool dev_res[N - K];
 
 __global__ void FindIncSeries_N(int k) {
 
+	int x = threadIdx.x;
 	int counter = 0;
-	while (counter < k - 1 && dev_numArray[threadIdx.x + counter] < dev_numArray[threadIdx.x + counter + 1]) {
+
+	while (counter < k - 1 && dev_numArray[x + counter] < dev_numArray[x + counter + 1]) {
 		counter++;
 	}
 	if (counter == k - 1)
-		dev_resIndex = threadIdx.x;
+		dev_resIndex = x;
 }
 
-__global__ void FindIncSeries_BLOCK() {
+__global__ void FindIncSeries_N_BLOCKS(int k) {
+
+	int x = threadIdx.x;
+	int bl_x = blockIdx.x;
+	int counter = 0;
+
+	if (bl_x == N / BLOCK_SIZE && x > N % BLOCK_SIZE - k)
+		return;
+
+	while (counter < k - 1 && dev_numArray[bl_x * BLOCK_SIZE + x + counter] < dev_numArray[bl_x * BLOCK_SIZE + x + counter + 1]) {
+		counter++;
+	}
+	if (counter == k - 1)
+		dev_resIndex = bl_x * BLOCK_SIZE + x;
+}
+
+__global__ void FindIncSeries_OneCicle() {
 	int x = threadIdx.x;
 	int y = threadIdx.y;
 	if (y == 0)
@@ -45,21 +63,30 @@ int main() {
 	{
 		numArray[i] = rand() % 10000;
 	}
+	numArray[10400] = 1;
+	numArray[10401] = 2;
+	numArray[10402] = 3;
+	numArray[10403] = 4;
+	numArray[10404] = 5;
+	numArray[10405] = 6;
+	numArray[10406] = 7;
+	numArray[10407] = 8;
+	numArray[10408] = 9;
+	numArray[10409] = 10;
 
 
 	cudaMemcpyToSymbol(dev_numArray, numArray, N * sizeof(int));
 	//FindIncSeries_N << < 1, N - K >> > (K);
-	//cudaMemcpyFromSymbol(&resIndex, dev_resIndex, sizeof(int));
+	FindIncSeries_N_BLOCKS << < N / BLOCK_SIZE + 1, BLOCK_SIZE >> > (K);
+	cudaMemcpyFromSymbol(&resIndex, dev_resIndex, sizeof(int));
 
-	FindIncSeries_BLOCK << < 1, dim3(N - K, K - 1) >> > ();
+	//FindIncSeries_OneCicle << < 1, dim3(N - K, K - 1) >> > ();
 	cudaError_t err = cudaGetLastError();
 	if (err != cudaSuccess) {
 		printf("CUDA error after kernel launch: %s\n", cudaGetErrorString(err));
 		return -1;
 	}
-	//FindIncSeries_BLOCK << <(N - 1) / Block_Size + 1, Block_Size >> > ();
-
-	cudaMemcpyFromSymbol(res, dev_res, (N - K) * sizeof(bool));
+	//cudaMemcpyFromSymbol(res, dev_res, (N - K) * sizeof(bool));
 
 	//CPU
 	/*int index = -1;
@@ -77,16 +104,16 @@ int main() {
 	}*/
 
 	//printing the nums
-	for (size_t i = 0; i < N; i++)
+	/*for (size_t i = 0; i < N; i++)
 	{
 		printf("%d:\t%d\n", i, numArray[i]);
-	}
-	for (size_t i = 0; i < N - K; i++)
+	}*/
+	/*for (size_t i = 0; i < N - K; i++)
 	{
 		if (res[i] == true)
 		{
 			printf("Ez egy jo index: %d\n", i);
 		}
-	}
-	//printf("Index, ahol %d szam novekvo sorrendben van egymas utan: %d.", K, resIndex);
+	}*/
+	printf("Index, ahol %d szam novekvo sorrendben van egymas utan: %d.", K, resIndex);
 }
