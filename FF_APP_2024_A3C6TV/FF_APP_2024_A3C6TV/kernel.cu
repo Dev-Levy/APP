ed16f9cd793bbd0bd2a4180d12c48b55b4188558
Oláh Levente <levente@stud.uni-obuda.hu>
2024-12-02 21:44:36 +0100

    update féléves

--- FF_APP_2024_A3C6TV/FF_APP_2024_A3C6TV/kernel.cu
+++ FF_APP_2024_A3C6TV/FF_APP_2024_A3C6TV/kernel.cu
@@ -11,9 +11,12 @@
 #include "sstream"
 #include "iostream"
 
+#define BLOCKSIZE 213
+
 int width, height, channels;
 unsigned char* img;
 unsigned char* dev_img;
+__device__ char dev_asciichars[] = "@%#*+=-:. ";
 
 
 void loadPNG(const char* imgpath) {
@@ -25,68 +28,70 @@ void loadPNG(const char* imgpath) {
 }
 void saveTXT() {
 	size_t pixelCount = width * height;
-	std::ofstream outputFile("csudakep.txt", std::ios::binary);
+	std::ofstream outputFile("csudakep.txt");
 
 	for (size_t i = 0; i < pixelCount; i++)
 	{
-		if (i % width == 0) {
+		if (i % width == 0 && i != 0) {
 			outputFile << std::endl;
 			std::cout << std::endl;
 		}
-
-		switch (img[i])
-		{
-		case 35:
-			outputFile << "#";
-			std::cout << static_cast<char>(35);
-			break;
-		case 176:
-			outputFile << "░";
-			std::cout << static_cast<char>(35);
-			break;
-		case 177:
-			outputFile << "▒";
-			std::cout << static_cast<char>(35);
-			break;
-		case 178:
-			outputFile << "▓";
-			std::cout << static_cast<char>(35);
-			break;
-		case 219:
-			outputFile << "█";
-			std::cout << static_cast<char>(35);
-			break;
-		default:
-			break;
-		}
+		outputFile << img[i];
+		std::cout << img[i];
 	}
 }
 
-__global__ void Pixels_To_ASCII_Kernel(unsigned char* d_img, int width, int height, int channels) {
-	int x = blockIdx.x * blockDim.x + threadIdx.x;
-	int y = blockIdx.y * blockDim.y + threadIdx.y;
+//__global__ void Pixels_To_ASCII_Kernel(unsigned char* d_img, int width, int height, int channels) {
+//	int x = blockIdx.x * blockDim.x + threadIdx.x;
+//	int y = blockIdx.y * blockDim.y + threadIdx.y;
+//
+//	if (x > width || y > height) return;
+//
+//	int idx = (y * width + x) * channels;
+//
+//	unsigned char r = d_img[idx + 0];
+//	unsigned char g = d_img[idx + 1];
+//	unsigned char b = d_img[idx + 2];
+//
+//	unsigned char gray = static_cast<unsigned char>(0.299f * r + 0.587f * g + 0.114f * b);
+//
+//	if (gray <= 51)
+//		d_img[idx] = 35; //hashtag #
+//	else if (gray > 51 && gray <= 102)
+//		d_img[idx] = 176; //light 
+//	else if (gray > 102 && gray <= 153)
+//		d_img[idx] = 177; //medium
+//	else if (gray > 153 && gray <= 204)
+//		d_img[idx] = 178; //dark
+//	else
+//		d_img[idx] = 219; //full
+//
+//}
+__global__ void imageToASCII(unsigned char* dev_img, int width, int height, int channels) {
+	int x = blockIdx.x * BLOCKSIZE + threadIdx.x;
 
-	if (x > width || y > height) return;
+	__shared__ char shr_asciiChars[10];
 
-	int idx = (y * width + x) * channels;
+	if (x == 0)
+	{
+		for (size_t i = 0; i < 10; i++)
+		{
+			shr_asciiChars[i] = dev_asciichars[i];
+		}
+	}
+	__syncthreads();
 
-	unsigned char r = d_img[idx + 0];
-	unsigned char g = d_img[idx + 1];
-	unsigned char b = d_img[idx + 2];
+	if (x > (width * height)) return;
 
-	unsigned char gray = static_cast<unsigned char>(0.299f * r + 0.587f * g + 0.114f * b);
+	int idx = x * 3;
+	float r = dev_img[idx] / 255.0f; //0-1 között
+	float g = dev_img[idx + 1] / 255.0f;
+	float b = dev_img[idx + 2] / 255.0f;
 
-	if (gray <= 51)
-		d_img[idx] = 35; //hashtag #
-	else if (gray > 51 && gray <= 102)
-		d_img[idx] = 176; //light 
-	else if (gray > 102 && gray <= 153)
-		d_img[idx] = 177; //medium
-	else if (gray > 153 && gray <= 204)
-		d_img[idx] = 178; //dark
-	else
-		d_img[idx] = 219; //full
+	float grayscale = 0.299f * r + 0.587f * g + 0.114f * b;
 
+	int index = (int)(grayscale * 9);
+	dev_img[x] = shr_asciiChars[index];
 }
 
 void Pixels_To_ASCII(unsigned char* img, int width, int height, int channels) {
@@ -97,10 +102,9 @@ void Pixels_To_ASCII(unsigned char* img, int width, int height, int channels) {
 	cudaMemcpy(dev_img, img, imgSize, cudaMemcpyHostToDevice);
 
 	//kernel call
-	dim3 blockSize(16, 16);
-	dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
-
-	Pixels_To_ASCII_Kernel << <gridSize, blockSize >> > (dev_img, width, height, channels);
+	int pixels = width * height;
+	int blocks = pixels / BLOCKSIZE + 1;
+	imageToASCII << <blocks, BLOCKSIZE >> > (dev_img, width, height, channels);
 	cudaDeviceSynchronize();
 	cudaError_t err = cudaGetLastError();
 
